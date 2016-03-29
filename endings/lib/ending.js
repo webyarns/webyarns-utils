bc2ed1895e1ba29bf64ed199d8773120703a9f3c
Raphael Parree <rparree@edc4it.com>
2016-03-29 14:27:11 +0200

    created the special endings

---
+++ endings/lib/ending.js
@@ -0,0 +1,16 @@
+$(function(){
+    $(".endingButton").click(function(e){
+
+        var $this = $(this);
+        var idx = $this.data("link");
+        var lastIdx= $this.data("last-idx");
+
+        $this.remove();
+        if ($(".endingButton").length==0){
+            var $2 = $(".slides>section:nth-child("+idx+")");
+            $2.data("next-idx",lastIdx)
+        }
+
+    })
+
+});
