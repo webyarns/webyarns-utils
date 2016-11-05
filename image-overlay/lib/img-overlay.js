e77eef7e18812205b77a2301e80cb240937364a6
Raphael Parree <rparree@edc4it.com>
2016-11-05 09:00:08 +0100

    pre-creating the 2nd backgrounds divs for slide-momevemtn speed

--- image-overlay/lib/img-overlay.js
+++ image-overlay/lib/img-overlay.js
@@ -1,6 +1,6 @@
 /**
  * section@data-overlay-background: adds a second background over the default on
- * use in conjunction with `section-id-to-bg-class` to set additional css styles such as
+ * use invar horizontalBackground  conjunction with `section-id-to-bg-class` to set additional css styles such as
  * Copies the transition from the default background. Can be overriden by using
  * section@data-background2-transition
  *
@@ -24,22 +24,43 @@ $(() => {
             const $bg = $(bg);
             const children = $bg.children("div.overlay-background");
             if (children.size() > 0) {
-                children.addClass("present");
-            } else {
-                const $2ndBg = $("<div class='slide-background present overlay-background'>").css({
-                    "display": "block",
-                    "background-image" : "url("+ bg2ImgValue + ")"
-                });
-                const bgTrValue = $bg.data( 'background-transition' );
-                const bg2TrValue =  $(e.currentSlide).data('background2-transition');
-                const transition = bg2TrValue || bgTrValue
-                if (transition !== undefined){
-                    $2ndBg.attr('data-background-transition',transition);
+
+                const $current = $(event.currentSlide);
+                if ($current.data("background2-replay") !== undefined) {
+
+                    children.each((i,e) => {
+                        const $child = $(e);
+                        const bgValue = "url(" + bg2ImgValue + "?load=" + new Date().getTime() + ")";
+                        $child.css("background-image", bgValue);
+                        console.log("changed to",$child.css("background-image"))
+                    })
+
                 }
-                $2ndBg.appendTo($bg);
+                children.addClass("present");
             }
         }
     }
+
+    $("section").each((i,e) => {
+        const $currentSlide = $(e);
+        const bg2ImgValue = $currentSlide.data('background2-image');
+        if (bg2ImgValue){
+            const bg = Reveal.getSlideBackground(i, 0);
+            const $bg = $(bg);
+            const $2ndBg = $("<div class='slide-background2 present overlay-background'>").css({
+                "display": "block",
+                "background-image" : "url("+ bg2ImgValue + ")"
+            });
+            const bgTrValue = $bg.data( 'background-transition' );
+            const bg2TrValue =  $currentSlide.data('background2-transition');
+            const transition = bg2TrValue || bgTrValue
+            if (transition !== undefined){
+                $2ndBg.attr('data-background-transition',transition);
+            }
+            $2ndBg.appendTo($bg);
+
+        }
+    });
 
     Reveal.addEventListener('ready', overlayHandler);
     Reveal.addEventListener('slidechanged', overlayHandler);
